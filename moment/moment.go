8f306c4dd22042650d29c776b4d788eadcebaed2
eloylp <eloylpdev@gmail.com>
2020-05-16 17:34:18 +0200

    Refactor type name

--- moment/moment.go
+++ moment/moment.go
@@ -4,9 +4,9 @@ import (
 	"time"
 )
 
-type Current func() time.Time
+type Now func() time.Time
 
-func NewFakedCurrent(year int, month time.Month, day, hour, min, sec, nSec int, loc *time.Location) Current {
+func NewFakedNow(year int, month time.Month, day, hour, min, sec, nSec int, loc *time.Location) Now {
 	return func() time.Time {
 		return time.Date(year, month, day, hour, min, sec, nSec, loc)
 	}
