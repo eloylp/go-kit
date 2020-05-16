3d1cf3076c786a9b568c530c87ba65fe22d5ef10
eloylp <eloylpdev@gmail.com>
2020-05-16 17:22:12 +0200

    Improve fanout bench tests

--- flow/fanout/fanout_bench_test.go
+++ flow/fanout/fanout_bench_test.go
@@ -6,42 +6,39 @@ import (
 	"github.com/eloylp/go-kit/flow/fanout/fanouttest"
 )
 
-func BufferedFanOutAddElem(b *testing.B, maxItems, subscriberBuffSize, subscribers, messagesSize int) {
+func BufferedFanOutAddElem(b *testing.B, subscribers, subscriberBuffSize, messagesSize int) {
 	b.ReportAllocs()
-	fo := fanouttest.PopulatedBufferedFanOut(b, 0, maxItems, subscriberBuffSize)
+	fo := fanouttest.BufferedFanOut(subscriberBuffSize)
 	for i := 0; i < subscribers; i++ {
 		fo.Subscribe()
 	}
 	b.ResetTimer()
 	data := make([]byte, messagesSize)
 	for n := 0; n < b.N; n++ {
-		err := fo.AddElem(data)
-		if err != nil {
-			b.Fatal(err)
-		}
+		fo.AddElem(data)
 	}
 }
 
-func BenchmarkBufferedFanOut_AddItem_10_10_100_2(b *testing.B) {
-	BufferedFanOutAddElem(b, 10, 10, 100, 2)
+func BenchmarkBufferedFanOut_AddItem_10_50_10000(b *testing.B) {
+	BufferedFanOutAddElem(b, 10, 50, 10000)
 }
 
-func BenchmarkBufferedFanOut_AddItem_10_10_1000_2(b *testing.B) {
-	BufferedFanOutAddElem(b, 10, 10, 1000, 2)
+func BenchmarkBufferedFanOut_AddItem_100_50_10000(b *testing.B) {
+	BufferedFanOutAddElem(b, 100, 50, 10000)
 }
 
-func BenchmarkBufferedFanOut_AddItem_100_100_10000_200(b *testing.B) {
-	BufferedFanOutAddElem(b, 100, 100, 10000, 200)
+func BenchmarkBufferedFanOut_AddItem_1000_50_10000(b *testing.B) {
+	BufferedFanOutAddElem(b, 1000, 50, 10000)
 }
 
-func BenchmarkBufferedFanOut_AddItem_100_100_10000_2(b *testing.B) {
-	BufferedFanOutAddElem(b, 100, 100, 10000, 2)
+func BenchmarkBufferedFanOut_AddItem_10000_50_10000(b *testing.B) {
+	BufferedFanOutAddElem(b, 10000, 50, 10000)
 }
 
-func BenchmarkBufferedFanOut_AddItem_100_100_100000_200(b *testing.B) {
-	BufferedFanOutAddElem(b, 100, 100, 100000, 200)
+func BenchmarkBufferedFanOut_AddItem_100000_50_10000(b *testing.B) {
+	BufferedFanOutAddElem(b, 100000, 50, 10000)
 }
 
-func BenchmarkBufferedFanOut_AddItem_100_100_1000000_200(b *testing.B) {
-	BufferedFanOutAddElem(b, 100, 100, 1000000, 200)
+func BenchmarkBufferedFanOut_AddItem_1000000_50_10000(b *testing.B) {
+	BufferedFanOutAddElem(b, 1000000, 50, 10000)
 }
