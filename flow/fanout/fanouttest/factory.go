db37422da78291d391e7eace73c32257dcabb4f0
eloylp <eloylpdev@gmail.com>
2020-05-17 16:59:26 +0200

    Remove dead code in tests

--- flow/fanout/fanouttest/factory.go
+++ flow/fanout/fanouttest/factory.go
@@ -6,15 +6,6 @@ import (
 	"strconv"
 )
 
-// PopulatedBufferedFanOut facilitates construction of an store
-// by accepting the number of elements to be present, and
-// store.BufferedStore needed constructor args.
-func PopulatedBufferedFanOut(elems, maxBuffLen int, now moment.Now) *fanout.BufferedFanOut {
-	fo := fanout.NewBufferedFanOut(maxBuffLen, now)
-	Populate(fo, elems)
-	return fo
-}
-
 func BufferedFanOut(maxBuffLen int, now moment.Now) *fanout.BufferedFanOut {
 	fo := fanout.NewBufferedFanOut(maxBuffLen, now)
 	return fo
