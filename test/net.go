818f1bbf2ee8b169f3350df06a61dece789a83bc
eloylp <eloylpdev@gmail.com>
2021-03-20 16:50:54 +0100

    Add WaitTCPService() function for waiting net services during tests

---
+++ test/net.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+)
+
+// WaitTCPService will try to connect to the provided TCP socket.
+// It will keep trying in the specified intervals for the specified
+// amount of time.
+//
+// It will declare the test as Failed, stopping the execution on fail.
+func WaitTCPService(t *testing.T, addr string, interval, maxWait time.Duration) {
+	t.Helper()
+	ctx, cancl := context.WithTimeout(context.Background(), maxWait)
+	defer cancl()
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fatalf("TryConnectTo(): %v", ctx.Err())
+		default:
+			con, conErr := net.Dial("tcp", addr)
+			if conErr == nil {
+				_ = con.Close()
+				return
+			}
+			time.Sleep(interval)
+		}
+	}
+}
