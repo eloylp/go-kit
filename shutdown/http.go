a901fca3d7142fccb3019d89966c44f11ba8cca8
eloylp <eloylpdev@gmail.com>
2021-04-30 23:52:12 +0200

    Fix comment

--- shutdown/http.go
+++ shutdown/http.go
@@ -13,8 +13,8 @@ import (
 // WithOSSignals will set up a OS signal watcher that will gracefully
 // shutdown the server when a proper signal is received.
 // If an error occurs, and a func(err error) is passed as last argument,
-// that will be used for handling the error. An already incremented wait group
-// is needed for wait for the shutdown method to finish.
+// that will be used for handling the error. Will auto increment the
+// passed *sync.WaitGroup.
 func WithOSSignals(s *http.Server, timeout time.Duration, wg *sync.WaitGroup, errHandler func(err error)) {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
