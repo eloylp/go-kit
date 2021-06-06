1b0ed9a9dd8d3f2a0b8187be506362cfcfa0a756
eloylp <eloylpdev@gmail.com>
2021-06-06 16:59:21 +0200

    Add exec package

---
+++ exec/exec.go
@@ -0,0 +1,27 @@
+package exec
+
+import (
+	"context"
+	"sync"
+)
+
+// Parallelize will execute the provided function with the maximum
+// specified level of parallelism until the context ends. Users of this
+// function can use the passed sync.WaitGroup to wait for termination of
+// all parallel tasks.
+func Parallelize(ctx context.Context, wg *sync.WaitGroup, maxConcurrent int, f func()) {
+	inProgressJobs := make(chan struct{}, maxConcurrent)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case inProgressJobs <- struct{}{}:
+			wg.Add(1)
+			go func() {
+				f()
+				<-inProgressJobs
+				wg.Done()
+			}()
+		}
+	}
+}
