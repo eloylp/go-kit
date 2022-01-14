63ecb0320ad6c0978c7f794edcf882a5da584440
eloylp <eloylp@protonmail.com>
2022-01-15 00:39:43 +0100

    Add WaitTLSService() test helper function

--- test/net.go
+++ test/net.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"context"
+	"crypto/tls"
 	"net"
 	"testing"
 	"time"
@@ -22,6 +23,36 @@ func WaitTCPService(t *testing.T, addr string, interval, maxWait time.Duration) 
 			t.Fatalf("WaitTCPService(): %v", ctx.Err())
 		default:
 			con, conErr := net.Dial("tcp", addr)
+			if conErr == nil {
+				_ = con.Close()
+				return
+			}
+			time.Sleep(interval)
+		}
+	}
+}
+
+// WaitTLSService will try to connect to the provided TLS socket.
+// It will keep trying in the specified intervals for the specified
+// amount of time.
+//
+// It will not do any X509 certificate verification as this is intended
+// to just try connectivity without getting SSL certificate checks errors
+// in logs.
+//
+// It will declare the test as Failed, stopping the execution on fail.
+func WaitTLSService(t *testing.T, addr string, interval, maxWait time.Duration) {
+	t.Helper()
+	ctx, cancl := context.WithTimeout(context.Background(), maxWait)
+	defer cancl()
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fatalf("WaitTLSService(): %v", ctx.Err())
+		default:
+			con, conErr := tls.Dial("tcp", addr, &tls.Config{
+				InsecureSkipVerify: true,
+			})
 			if conErr == nil {
 				_ = con.Close()
 				return
