28ba5d3f354740dac5af858ac55c1aec9b9988d0
eloylp <eloylp@eloylp.dev>
2023-01-05 14:12:11 +0100

    refine comments

--- flow/fanout/fanout.go
+++ flow/fanout/fanout.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Slot represents an enqueueable element. Timestamp
-// will allow consumers discard old messages. T will
+// will allow consumers discard old elements. T will
 // represent the user custom data.
 type Slot[T any] struct {
 	TimeStamp time.Time
@@ -19,34 +19,36 @@ type Slot[T any] struct {
 // It should be called by users in order
 // to retrieve elements from the queue.
 //
-// In case there are no more elements, an io.EOF
-// error will be returned.
+// In case there are no more elements or the subscription
+// is cancelled, an io.EOF error will be returned.
 type ConsumerFunc[T any] func() (*Slot[T], error)
 
-// CancelFunc is the main way that a consumer can end its
-// subscription. When called, the subscriber channel
-// will be closed, so consumer will still try to
-// consume all the remaining messages.
+// CancelFunc is the way that a consumer can terminate
+// its subscription. When called, the subscriber
+// channel will be closed, so consumer could still try to
+// consume all the remaining elements.
 type CancelFunc func() error
 
 // Status represents a report about how much
 // elements are queued per consumer.
 //
-// As the Key, the provided subscriber UUID is used (See BufferedFanOut.SubscribeWith).
+// As the Key, the user provided subscriber UUID is used.
 // If not provided, an empty string will be used, aggregating
 // all counter values from all non customized consumers.
 //
-// As Value, the number of queued elements.
+// As the Value, the number of queued elements.
 type Status map[string]int
 
-// BufferedFanOut represent a fan-out pattern making
-// use of channels.
+// BufferedFanOut represents a fan-out in-memory pattern
+// with a configurable buffer.
 //
 // It Will send a copy of the element to multiple
-// subscribers at the same time.
+// subscribers at the same time. In case a consumer
+// gets stalled, older elements will be discarded
+// in favour of the new arrived ones.
 //
-// This implements the needed locking mechanisms
-// to be considered thread safe.
+// This implements all the needed locking mechanisms,
+// so it can be considered thread safe.
 type BufferedFanOut[T any] struct {
 	subscribers []*subscriber[T]
 	maxBuffLen  int
@@ -58,9 +60,7 @@ type subscriber[T any] struct {
 	UUID string
 }
 
-// NewBufferedFanOut needs buffer size for subscribers channels
-// and function that must retrieve the current time for Slots
-// timestamps.
+// NewBufferedFanOut is the constructor for BufferedFanOut.
 func NewBufferedFanOut[T any](maxBuffLen int) *BufferedFanOut[T] {
 	fo := &BufferedFanOut[T]{
 		maxBuffLen: maxBuffLen,
@@ -68,13 +68,7 @@ func NewBufferedFanOut[T any](maxBuffLen int) *BufferedFanOut[T] {
 	return fo
 }
 
-// Add will send of elem to all subscribers channels.
-// Take care about race conditions and the type of
-// element that pass in. If you pass an integer, that
-// will be copied to each subscriber. Problem comes if you
-// pass referenced types like maps or slices or any other
-// pointer type.
-//
+// Add will send an elem to all subscribers channels.
 // If one of the subscribers channels is full, oldest data
 // will be discarded.
 func (fo *BufferedFanOut[T]) Add(elem T) {
@@ -99,7 +93,7 @@ func (fo *BufferedFanOut[T]) publish(sl *Slot[T]) {
 	}
 }
 
-// ActiveSubscribers can tell us how many subscribers
+// ActiveSubscribers will tell us how many subscribers
 // are registered and active in the present moment.
 func (fo *BufferedFanOut[T]) ActiveSubscribers() int {
 	fo.L.RLock()
@@ -115,24 +109,33 @@ func (fo *BufferedFanOut[T]) ActiveSubscribers() int {
 
 // SubscribersLen can tell us the size of the underlying
 // subscriber storage. This will return both, active and
-// non active slots.
+// non active (free) subscriber slots.
 func (fo *BufferedFanOut[T]) SubscribersLen() int {
 	fo.L.RLock()
 	defer fo.L.RUnlock()
 	return len(fo.subscribers)
 }
 
-// Subscribe will return an output channel that will
-// be filled when more data arrives this fanout. Also,
-// a cancelFunc, for easily canceling the subscription.
+// Subscribe will return two functions, a ConsumerFunc and
+// a CancelFunc (see types definitions).
 //
-// If you are not actively consuming this channel, but
-// data continues arriving to the fanout, the oldest
-// element will be dropped in favor of the new one.
+// The ConsumerFunc will be used for pulling data from the
+// fanout system, whereas the CancelFunc should always be
+// used when the consumer its not interested on continuing
+// its activity.
+//
+// Its IMPORTANT to not forget to use the CancelFunc to cancel
+// the consumer activity. If not, resources could be leaked. A
+// good practice here would be to always call the CancelFunc
+// in a defer statement.
 func (fo *BufferedFanOut[T]) Subscribe() (ConsumerFunc[T], CancelFunc) { //nolint:gocritic
 	return fo.SubscribeWith("")
 }
 
+// SubscribeWith is same as Subscribe, but it allows to
+// customize the subscriber with an UUID. Which might
+// unequivocally identify a subscriber or a group of them
+// in the system.
 func (fo *BufferedFanOut[T]) SubscribeWith(uuid string) (ConsumerFunc[T], CancelFunc) { //nolint:gocritic
 	fo.L.Lock()
 	defer fo.L.Unlock()
@@ -185,7 +188,10 @@ func (fo *BufferedFanOut[T]) unsubscribe(index int) error {
 
 // Reset will clear all the data and
 // subscribers, starting again. It will
-// also close all the subscribers channels.
+// also close all the subscribers internally.
+//
+// After calling Reset(), Subscribers can still
+// consume all the remaining elements.
 func (fo *BufferedFanOut[T]) Reset() {
 	fo.L.Lock()
 	defer fo.L.Unlock()
@@ -199,7 +205,7 @@ func (fo *BufferedFanOut[T]) Reset() {
 }
 
 // Status will return a Status type with
-// the list of all subscribers and they
+// the list of all subscribers and their
 // pending elements.
 func (fo *BufferedFanOut[T]) Status() Status {
 	fo.L.RLock()
