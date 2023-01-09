4793ddf865e030119e721d0893d9549a3d3fe54a
eloylp <eloylp@eloylp.dev>
2023-01-09 23:53:13 +0100

    refactor: be consistent in naming

--- moment/moment.go
+++ moment/moment.go
@@ -9,15 +9,15 @@ import (
 // package will support.
 const layout = "2006-01-02 15:04:05"
 
-// Now represents a function that returns current time.
+// NowFunc represents a function that returns current time.
 // The target is to decouple time gathering operation
 // from production code and in the manner, facilitate time
 // based tests. This type can be used in structs, even as a function parameter.
-type Now func() time.Time
+type NowFunc func() time.Time
 
 // NewFakedNow returns a faked Now type
 // ready to be used in tests.
-func NewFakedNow(t *testing.T, date string) Now {
+func NewFakedNow(t *testing.T, date string) NowFunc {
 	d, err := time.Parse(layout, date)
 	if err != nil {
 		t.Fatal(err)
@@ -30,7 +30,7 @@ func NewFakedNow(t *testing.T, date string) Now {
 // NewFakedNowWithLoc returns a faked Now type
 // ready to be used in tests. Needs a location as second
 // parameter.
-func NewFakedNowWithLoc(t *testing.T, date string, loc *time.Location) Now {
+func NewFakedNowWithLoc(t *testing.T, date string, loc *time.Location) NowFunc {
 	d, err := time.ParseInLocation(layout, date, loc)
 	if err != nil {
 		t.Fatal(err)
