2c41613c676816a997c05f7df5017a6dc5726f0d
eloylp <eloylp@eloylp.dev>
2023-01-12 19:55:24 +0100

    Make handler tester accept *testing.T as argument

--- test/handler/tester.go
+++ test/handler/tester.go
@@ -33,29 +33,27 @@ type TestAuxFunc func(t *testing.T)
 // passed as parameters, that need to include the setup and teardown logic. Probably,
 // this functions will need to implement t.Fatal() to not continue executing current test.
 // For the TestAuxFunc you can still pass nil , if yo dont have any logic for them.
-func Tester(cases []Case, router http.Handler, setUp, tearDown TestAuxFunc) func(t *testing.T) {
-	return func(t *testing.T) {
-		if setUp != nil {
-			setUp(t)
-		}
-		for _, tt := range cases {
-			name := fmt.Sprintf("path: %s, method: %s, case: %q", tt.Path, tt.Method, tt.Case)
-			t.Run(name, func(t *testing.T) {
-				rec, req := httptest.NewRecorder(), httptest.NewRequest(tt.Method, tt.Path, tt.Body) //nolint:scopelint
-				req.Header = tt.Headers                                                              //nolint:scopelint
-				router.ServeHTTP(rec, req)
-				res := rec.Result() //nolint:bodyclose
-				body, err := ioutil.ReadAll(res.Body)
-				if err != nil {
-					t.Fatal(err)
-				}
-				for _, chk := range tt.Checkers { //nolint:scopelint
-					chk(t, res, body)
-				}
-			})
-		}
-		if tearDown != nil {
-			tearDown(t)
-		}
+func Tester(t *testing.T, cases []Case, router http.Handler, setUp, tearDown TestAuxFunc) {
+	if setUp != nil {
+		setUp(t)
+	}
+	for _, tt := range cases {
+		name := fmt.Sprintf("path: %s, method: %s, case: %q", tt.Path, tt.Method, tt.Case)
+		t.Run(name, func(t *testing.T) {
+			rec, req := httptest.NewRecorder(), httptest.NewRequest(tt.Method, tt.Path, tt.Body) //nolint:scopelint
+			req.Header = tt.Headers                                                              //nolint:scopelint
+			router.ServeHTTP(rec, req)
+			res := rec.Result() //nolint:bodyclose
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			for _, chk := range tt.Checkers { //nolint:scopelint
+				chk(t, res, body)
+			}
+		})
+	}
+	if tearDown != nil {
+		tearDown(t)
 	}
 }
